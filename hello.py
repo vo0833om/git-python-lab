f8cde7a0aebbc53a5d7651f6f04f374d5e537dbc
unknown <grant.anderson29@yahoo.com>
2018-09-17 14:46:47 -0500

    my name and another thing

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 def main():
     print('Hello class, My Name is Roman Martushev!')
+	print( 'Hello my name is grant and they should invent more awesome hurricane names. psssh florence...that doesnt scare me!')
 
 main()
